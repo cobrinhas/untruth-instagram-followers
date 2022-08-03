aefa4c403cf36c8eead202ae4e50f039643b8321
freitzzz <1160907@isep.ipp.pt>
2022-08-03 20:00:31 +0100

    remove tests

--- test/untruth_instagram_followers_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:untruth_instagram_followers/untruth_instagram_followers.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
