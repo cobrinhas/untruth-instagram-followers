6f34de617bb4a85e727ab03d787c7587338fefff
freitzzz <1160907@isep.ipp.pt>
2022-08-03 13:05:56 +0100

    dart console app

---
+++ bin/untruth_instagram_followers.dart
@@ -0,0 +1,5 @@
+import 'package:untruth_instagram_followers/untruth_instagram_followers.dart' as untruth_instagram_followers;
+
+void main(List<String> arguments) {
+  print('Hello world: ${untruth_instagram_followers.calculate()}!');
+}
---
+++ test/untruth_instagram_followers_test.dart
@@ -0,0 +1,8 @@
+import 'package:untruth_instagram_followers/untruth_instagram_followers.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
