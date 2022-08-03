6901a298dc224eee24c2fb2f268dd32f3a2acdbf
freitzzz <1160907@isep.ipp.pt>
2022-08-03 16:43:41 +0100

    cookies press + login form submit done

--- bin/untruth_instagram_followers.dart
+++ bin/untruth_instagram_followers.dart
@@ -1,19 +1,63 @@
 import 'package:puppeteer/puppeteer.dart';
 
+const cookiesButtonSelectorExpression =
+    "//button[contains(., 'Permitir apenas cookies essenciais')]";
+
+const loginFormUsernameInputSelectorExpression = "input[name='username']";
+const loginFormPasswordInputSelectorExpression = "input[name='password']";
+const loginFormSubmitButtonSelectorExpression = "button[type='submit']";
+
+const inputTypeDelayDuration = Duration(milliseconds: 50);
+
 void main() async {
   // Download the Chromium binaries, launch it and connect to the "DevTools"
-  var browser = await puppeteer.launch();
+  final browser = await puppeteer.launch(
+    headless: false,
+  );
 
   // Open a new tab
-  var myPage = await browser.newPage();
+  final myPage = await browser.newPage();
 
   // Go to a page and wait to be fully loaded
-  await myPage.goto('https://dart.dev', wait: Until.networkIdle);
+  await myPage.goto('https://instagram.com', wait: Until.networkIdle);
 
-  // Do something... See other examples
-  await myPage.screenshot();
-  await myPage.pdf();
-  await myPage.evaluate('() => document.title');
+  final cookiesButtonElements = await myPage.$x(
+    cookiesButtonSelectorExpression,
+  );
+
+  if (cookiesButtonElements.isNotEmpty) {
+    final cookiesButtonElement = cookiesButtonElements.first;
+
+    await cookiesButtonElement.click();
+  }
+
+  await Future.delayed(Duration(seconds: 2));
+
+  final loginFormUsernameInputElement = await myPage.$(
+    loginFormUsernameInputSelectorExpression,
+  );
+
+  await loginFormUsernameInputElement.type(
+    'username example',
+    delay: inputTypeDelayDuration,
+  );
+
+  final loginFormPasswordInputElement = await myPage.$(
+    loginFormPasswordInputSelectorExpression,
+  );
+
+  await loginFormPasswordInputElement.type(
+    'password',
+    delay: inputTypeDelayDuration,
+  );
+
+  final loginFormSubmitButtonElement = await myPage.$(
+    loginFormSubmitButtonSelectorExpression,
+  );
+
+  await loginFormSubmitButtonElement.tap();
+
+  await Future.delayed(Duration(seconds: 3));
 
   // Gracefully close the browser's process
   await browser.close();
