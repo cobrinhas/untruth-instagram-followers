94d4c04013875a6cedb399fdaa5eeb17dd26d72d
freitzzz <1160907@isep.ipp.pt>
2022-08-20 12:05:03 +0100

    delete env.dart

--- lib/env.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:io';
-
-final username = Platform.environment['instagram_username']!;
-final password = Platform.environment['instagram_password']!;
-final profileId = Platform.environment['instagram_profile_id']!;
